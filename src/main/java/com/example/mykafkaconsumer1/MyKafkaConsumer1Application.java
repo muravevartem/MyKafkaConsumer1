3d1137025401fee8ffc331195a5f065136601953
muravevartem <muravevartem@outlook.com>
2022-10-15 17:54:45 +0400

    Initial commit

--- src/main/java/com/example/mykafkaconsumer1/MyKafkaConsumer1Application.java
+++ src/main/java/com/example/mykafkaconsumer1/MyKafkaConsumer1Application.java
@@ -2,12 +2,18 @@ package com.example.mykafkaconsumer1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class MyKafkaConsumer1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MyKafkaConsumer1Application.class, args);
+    }
+
+    @KafkaListener(topics = "test-topic", id = "my-kafka-conusmer")
+    public void listen(TestModel model) {
+        System.out.println(model);
     }
 
 }
--- src/main/java/com/example/mykafkaconsumer1/TestEnum.java
+++ src/main/java/com/example/mykafkaconsumer1/TestEnum.java
@@ -1,2 +1,7 @@
-package com.example.mykafkaconsumer1;public enum TestEnum {
+package com.example.mykafkaconsumer1;
+
+public enum TestEnum {
+    TEST_ENUM_1,
+    TEST_ENUM_2,
+    TEST_ENUM_3
 }
